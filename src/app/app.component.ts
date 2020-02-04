688fdcd4f1602a16bc59c3b6f1c1bff396de0677
Derek Holmes <derek.holmes@amadeus.com>
2020-02-04 08:16:42 -0700

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // Describe what this is doing
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  data = [];
+
+  // Documentation at: http://navitaireinterviewapp.azurewebsites.net/swagger/ui/index.html
+  readonly flightsUrl =
+    'http://navitaireinterviewapp.azurewebsites.net/api/Flights';
+
+  constructor(protected http: HttpClient) {}
+
+  async ngOnInit() {
+    // Difference between a promise and an observable?
+    this.data = await this.http.get<any>(this.flightsUrl).toPromise();
+  }
 }
